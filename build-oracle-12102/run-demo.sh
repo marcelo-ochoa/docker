76ffd9bc2b049eefe08b617e7eb0383e2ae7f3b1
Marcelo Ochoa <mochoa@temperies.com>
2016-06-09 14:52:40 -0300

    updated some sample directories on .sh scripts

--- build-oracle-12102/run-demo.sh
+++ build-oracle-12102/run-demo.sh
@@ -1,1 +1,1 @@
-docker run --privileged=true --volume=/var/lib/docker/db/demo:/u01/app/oracle/data --name demo --hostname demo --detach=true --publish=1521:1521 oracle-rdbms:12.1.0.2
+docker run --privileged=true --volume=/home/data/db/demo:/u01/app/oracle/data --name demo --hostname demo --detach=true --publish=1521:1521 oracle-rdbms:12.1.0.2
--- build-oracle-12102/run.sh
+++ build-oracle-12102/run.sh
@@ -1,1 +1,1 @@
-docker run --privileged=true --volume=/var/lib/docker/db/ols:/u01/app/oracle/data --name ols --hostname ols --detach=true --publish=1521:1521 --publish=9099:9099 oracle-rdbms:12.1.0.2
+docker run --privileged=true --volume=/home/data/db/ols:/u01/app/oracle/data --name ols --hostname ols --detach=true --publish=1521:1521 oracle-rdbms:12.1.0.2
