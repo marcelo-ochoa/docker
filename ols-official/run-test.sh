aec22d0aa457be840dfcece9b2f61adab2557e3c
Marcelo Ochoa <mochoa@temperies.com>
2017-05-17 10:39:40 -0300

    Added shm parameter as example in run command

--- ols-official/run-test.sh
+++ ols-official/run-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -d --privileged=true --name test --hostname test \
+docker run -d --privileged=true --name test --hostname test --shm-size=1g \
 -p 1521:1521 -p 5500:5500 -p 9099:9099 \
 -e ORACLE_SID=TEST \
 -e ORACLE_PDB=PDB1 \
