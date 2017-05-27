25d20d6dcb75aae123e138e908633574b7ba99d2
Marcelo Ochoa <mochoa@temperies.com>
2017-05-27 17:31:08 -0300

    removed ORACLE_PWD example and add timezone sample setting

--- ols-official/run-test.sh
+++ ols-official/run-test.sh
@@ -3,6 +3,6 @@ docker run -d --privileged=true --name test --hostname test --shm-size=1g \
 -p 1521:1521 -p 5500:5500 -p 9099:9099 \
 -e ORACLE_SID=TEST \
 -e ORACLE_PDB=PDB1 \
--e ORACLE_PWD="Oracle2017!" \
+-v /etc/timezone:/etc/timezone:ro -e TZ="America/Argentina/Buenos_Aires" \
 -v /home/data/db/test:/opt/oracle/oradata \
 ols-official:2.0.1
